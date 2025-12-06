69ff7a8ef6094bfc634ccf1197a35fb3329eedfc
Chivano van Rijsdam <82022267+Chivano295@users.noreply.github.com>
2025-12-06 18:12:05 +0100

    section 4

--- game/Ballgame/Assets/Scripts/playerstuck.cs
+++ game/Ballgame/Assets/Scripts/playerstuck.cs
@@ -1,16 +1,39 @@
+using UnityEditor.AnimatedValues;
 using UnityEngine;
 
 public class playerstuck : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject playerball;
+    [SerializeField] Vector3 playerpos;
+    [SerializeField] float maxtime;
+    [SerializeField] float timed;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float rbforce;
+    [SerializeField] float offsetDistance;
+    
+   
+
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {       
+
+        if(timed > 0)
+        {
+            timed -= Time.deltaTime;
+        }
+        else
+        {
+            timed = maxtime;
+
+            float distance = Vector3.Distance(transform.position, playerpos);
+            Debug.Log(distance);
+            if (distance < offsetDistance)
+            {
+                rb.AddForce(Vector2.up * rbforce);
+            }
+            playerpos = transform.position;
+            
+        }
     }
 }
