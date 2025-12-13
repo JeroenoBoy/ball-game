44998be6858ba7f025136b0e202e3e3ede21826a
Jeroen <jeroen@vandegeest.eu>
2025-12-13 12:22:00 +0100

    tracking help

---
+++ game/Ballgame/Assets/Scripts/PlayerToTrack.cs
@@ -0,0 +1,38 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+
+public class PlayerToTrack : MonoBehaviour {
+
+    [SerializeField] private Transform playerParent;
+    
+    private CinemachineCamera cinemachineCamera;
+
+    private void Awake() {
+        cinemachineCamera = GetComponent<CinemachineCamera>();
+    }
+
+    private void FixedUpdate() {
+        Transform closestPlayer = null;
+        float playerDistance = float.MaxValue;
+        
+        foreach (object o in playerParent) {
+            Transform playerTransform = o as Transform;
+            if (playerTransform.position.y > playerDistance) {
+                continue;
+            }
+
+            closestPlayer = playerTransform;
+            playerDistance = playerTransform.position.y;
+        }
+
+        if (closestPlayer == null) {
+            return;
+        }
+
+        Vector3 pos = closestPlayer.position;
+        pos.z = -10;
+        cinemachineCamera.Target.TrackingTarget = closestPlayer;
+    }
+}
