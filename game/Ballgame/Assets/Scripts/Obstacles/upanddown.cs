f9b3155893697125b8f48903af4f3c25b37eea4d
Chivano van Rijsdam <82022267+Chivano295@users.noreply.github.com>
2025-12-14 21:20:16 +0100

    LEVEL added

--- game/Ballgame/Assets/Scripts/Obstacles/upanddown.cs
+++ game/Ballgame/Assets/Scripts/Obstacles/upanddown.cs
@@ -7,6 +7,7 @@ public class upanddown : MonoBehaviour
     [SerializeField] private float moveDistance = 2f;
     [SerializeField] private float movespeed = 1f;
     [SerializeField] private Ease easeType = Ease.InOutSine;
+    [SerializeField] bool moveX;
 
     private Vector3 startPos;
 
@@ -15,7 +16,15 @@ public class upanddown : MonoBehaviour
     {
         startPos = transform.position;
 
-        transform.DOMoveY(startPos.y + moveDistance, movespeed).SetEase(easeType).SetLoops(-1, LoopType.Yoyo);
+        if (!moveX)
+        {
+            transform.DOMoveY(startPos.y + moveDistance, movespeed).SetEase(easeType).SetLoops(-1, LoopType.Yoyo);
+        }
+        else
+        {
+            transform.DOMoveX(startPos.x + moveDistance, movespeed).SetEase(easeType).SetLoops(-1, LoopType.Yoyo);
+        }
+        
     }
 
 }
